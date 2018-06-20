2b28751347947c32b6470abdec9bbcd48c8aad30
Greg Garville Jr <gjgarvil@gmail.com>
2018-06-20 16:34:23 -0400

    added signuser resolver, fixed un constant erro

--- app/graphql/types/mutation_type.rb
+++ app/graphql/types/mutation_type.rb
@@ -2,4 +2,5 @@ class Types::MutationType < Types::BaseObject
   
   field :createLink, function: Resolvers::CreateLink.new
   field :createUser, function: Resolvers::CreateUser.new
+  field :signUser, function: Resolvers::SignUser.new
 end
