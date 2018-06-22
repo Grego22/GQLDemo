52a710e286a29df1eb258261cf0e0cdeeac72342
Greg Garville Jr <gjgarvil@gmail.com>
2018-06-22 10:28:31 -0400

    migrated db, and began coding invoice resolver

--- app/graphql/types/mutation_type.rb
+++ app/graphql/types/mutation_type.rb
@@ -3,4 +3,5 @@ class Types::MutationType < Types::BaseObject
   field :createLink, function: Resolvers::CreateLink.new
   field :createUser, function: Resolvers::CreateUser.new
   field :signUser, function: Resolvers::SignUser.new
+  field :Invoice, function: Resolvers::Invoice.new
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_06_20_180411) do
+ActiveRecord::Schema.define(version: 2018_06_21_190540) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "invoices", force: :cascade do |t|
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "links", force: :cascade do |t|
     t.string "url"
