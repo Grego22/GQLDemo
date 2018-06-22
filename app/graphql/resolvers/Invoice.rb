2cf46f7fdcbf62ec296120fc2f8c60dd2b96b35a
Greg Garville Jr <gjgarvil@gmail.com>
2018-06-22 11:19:51 -0400

    added invoice to query_type

--- app/graphql/resolvers/Invoice.rb
+++ app/graphql/resolvers/Invoice.rb
@@ -1,13 +1,14 @@
 class Resolvers::Invoice<GraphQl::Function
-    field :fee_in_cents, !types.Int
+    argument :fee_in_cents, !types.Int
 
+   
 
 
     type Types::InvoiceType
+
     def call(_obj, args, _ctx)
         Invoice.create!(
-          description: args[:description],
-          url: args[:url],
+         
         )
     end
 end
--- app/graphql/types/Invoice_type.rb
+++ app/graphql/types/Invoice_type.rb
@@ -1,6 +1,9 @@
-InvoiceType = GraphQL::ObjectType.define do
-    name 'Invoice'
-    description 'An Invoice'
+Types::InvoiceType = GraphQL::ObjectType.define do
+    
+  name 'Invoice'
+  
+  
+
     field :id, !types.Int
     field :fee_in_cents, types.Int
   end
